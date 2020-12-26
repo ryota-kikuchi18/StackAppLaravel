eb8254060a49ff563dfc4cc6b1b3e104f14a2b45
“ryota.kikuchi” <r.s.10180830@gmail.com>
2020-12-26 16:58:50 +0900

    改善: email.blade.phpでサイドバーを作るための準備

--- resources/views/settings/email.blade.php
+++ resources/views/settings/email.blade.php
@@ -8,31 +8,40 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-    <title>Document</title>
+    <title>設定画面</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <div class="container">
+    <div class=".container-fluid">
         <header>
             <p class="h2 text-center">設定</p>
         </header>
     </div>
     <hr>
-    <div class="container">
-        <section>
-            <form action="" method="">
-                <div class="form-group">
-                    <label for="email">EMAIL</label>
-                    <input class="form-control" type="email" name="email" id="email" placeholder="new EMAIL">
-                </div>
-                <button type="submit" class="btn btn-dark mb-2">登録</button>
-            </form>
-        </section>
+    <div class=".container-fluid">
+        <div class="row">
+            <div class="box1 col-2">
+                <aside>
+                    <p>サイトバー</p>
+                </aside>
+            </div>
+            <div class="box2 col-10">
+                <section>
+                    <form action="" method="">
+                        <div class="form-group">
+                            <label for="email">EMAIL</label>
+                            <input class="form-control" type="email" name="email" id="email" placeholder="new EMAIL">
+                        </div>
+                        <button type="submit" class="btn btn-dark mb-2">登録</button>
+                    </form>
+                </section>
+            </div>
+        </div>
     </div>
     <hr>
-    <div class="container">
+    <div class=".container-fluid">
         <footer>
             <p>&copy;mokumoku</p>
         </footer>
